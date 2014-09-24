fd698f71b7b6de7ba7d48ace4abc791ac76b0dc2
otictac1 <bradleydunlap35@gmail.com>
2014-09-24 17:13:00 -0400

    Small Change

--- Expenses/src/com/mongoose/studios/expenses/MainActivity.java
+++ Expenses/src/com/mongoose/studios/expenses/MainActivity.java
@@ -191,8 +191,7 @@ public class MainActivity extends Activity implements View.OnClickListener,
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+	public boolean onCreateOptionsMenu(Menu menu) {		
 		abtv = new TextView(this);
 		abtv.setText(getString(R.string.balance) + " " + balance);
 		abtv.setPadding(5, 0, 10, 0);
@@ -206,10 +205,7 @@ public class MainActivity extends Activity implements View.OnClickListener,
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
+	public boolean onOptionsItemSelected(MenuItem item) {		
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 
